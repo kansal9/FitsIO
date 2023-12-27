cd51ff86e030e4faf2071d9df05185bf9d0cb6a7
Vanshika Kansal <vanshikakansal@gmail.com>
2023-12-27 13:11:56 +0100

    added image functionality

--- FitsFile/Image.py
+++ FitsFile/Image.py
@@ -1,17 +1,11 @@
-#/usr/bin/env python3
+# /usr/bin/env python3
 # coding: utf-8
 
-import os
-import warnings
-
-import numpy as np
-
-from astropy.io import fits
 from skimage.transform import resize
+import numpy as np
+from Hdu import *
 
-from .Hdu import *
-
-__all__ = ['FitsImage',]
+__all__ = ['FitsImage']
 
 
 class FitsImage(FitsHDU):
@@ -34,6 +28,36 @@ class FitsImage(FitsHDU):
     def read_image_pixels(self):
         return self.hdulist[self.hdu_index].data.size
 
+    def scale_image(self, factor):
+        return self.hdulist[self.hdu_index].data * factor
+
+    def shift_image(self, factor):
+        return self.hdulist[self.hdu_index].data + factor
+
+    def dump_row(self, row_no, axis=0):
+        # Dumps the pixel values of a specified row of the image
+        new_data = np.delete(self.hdulist[self.hdu_index].data, row_no, axis=axis)
+        return new_data
+
+    def transpose(self):
+        new_data = np.transpose(self.hdulist[self.hdu_index].data)
+        return new_data
+
+    def sum_row(self, row_idx, data_range=None):
+        data = self.hdulist[self.hdu_index].data
+        if data_range is None:
+            # summing the entire row
+            val = data[row_idx, :].sum()
+        else:
+            # summing within a specified range
+            start, end = data_range
+            val = data[row_idx, start:end + 1].sum()
+        return val
+
+    def sum_col(self, col_idx):
+        val = self.hdulist[self.hdu_index].data[:, col_idx].sum()
+        return val
+
     def redefine_size(self, scaling_factor):
         new_shape = tuple(map(lambda x: int(x * scaling_factor), self.hdulist[0].data.shape))
         new_data = resize(self.hdulist[0].data, new_shape)
@@ -45,5 +69,3 @@ class FitsImage(FitsHDU):
 
     def read_segment(self, start, stop):
         return self.hdulist[self.hdu_index].data[start[0]:stop[0], start[1]:stop[1]]
-
-
